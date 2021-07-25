a0ff7145ba91c8a1f60e7984b5ca63916b3698da
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-07-25 10:47:37 +0530

    creating a list for scorekeeper

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -50,9 +62,12 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: ElevatedButton(
+              style:  ElevatedButton.styleFrom(
+                primary: Colors.green,
+
+              ),
+
               child: Text(
                 'True',
                 style: TextStyle(
@@ -69,8 +84,11 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: ElevatedButton(
+            style:  ElevatedButton.styleFrom(
+              primary: Colors.red,
+              ),
+
               child: Text(
                 'False',
                 style: TextStyle(
@@ -84,7 +102,10 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
+
       ],
     );
   }
